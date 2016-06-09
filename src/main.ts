387028b9d7c6ed8bc20261a8c9a7e31444d2d19f
drkchiloll <samwomack_@me.com>
2016-06-09 17:55:45 -0500

    keep only items needed for this project

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,14 @@
 import { enableProdMode } from '@angular/core';
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import { ELEMENT_PROBE_PROVIDERS } from '@angular/platform-browser';
-import { ROUTER_PROVIDERS } from '@angular/router-deprecated';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
 import { HTTP_PROVIDERS } from '@angular/http';
 
-import { Store } from '@ngrx/store';
 
 import { App } from './components/app';
 
-const ENV_PROVIDERS = [];
-// depending on the env mode, enable prod mode or add debugging modules
-if (process.env.ENV === 'build') {
-  enableProdMode();
-} else {
-  ENV_PROVIDERS.push(ELEMENT_PROBE_PROVIDERS);
-}
 
 bootstrap(App, [
     // These are dependencies of our App
     ...HTTP_PROVIDERS,
-    ...ROUTER_PROVIDERS,
-    ...ENV_PROVIDERS,
-    { provide: LocationStrategy, useClass: HashLocationStrategy } // use #/ routes, remove this for HTML5 mode
   ])
   .catch(err => console.error(err));
