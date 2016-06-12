a6d586eca19500f90a8879f4113bbf4de8985824
drkchiloll <samwomack_@me.com>
2016-06-12 16:02:42 -0500

    These are the Action Types for our Reducer Function

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const SELECT_ITEM = 'SELECT_ITEM';
+export const ADD_ITEMS = 'ADD_ITEMS';
+export const CREATE_ITEM = 'CREATE_ITEM';
+export const UPDATE_ITEM = 'UPDATE_ITEM';
+export const DELETE_ITEM = 'DELETE_ITEM';
